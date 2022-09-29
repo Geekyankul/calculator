ba19b79cc68e85420cfafca86d195bc3755992e9
Geekyankul <76466607+Geekyankul@users.noreply.github.com>
2022-09-29 12:22:38 +0530

    update

--- script.js
+++ script.js
@@ -1,27 +1,22 @@
-let screen = document.getElementById('screen');
-buttons = document.querySelectorAll('button');
-let screenValue = '';
-for (item of buttons) {
-    item.addEventListener('click', (e) => {
-        buttonText = e.target.innerText;
-        console.log('Button text is ', buttonText);
-        if (buttonText == 'X') {
-            buttonText = '*';
-            screenValue += buttonText;
-            screen.value = screenValue;
-        }
-        else if (buttonText == 'C') {
-            screenValue = "";
-            screen.value = screenValue;
-        }
-        else if (buttonText == '=') {
-            screen.value = eval(screenValue);
-        }
-        else {
-            screenValue += buttonText;
-            screen.value = screenValue;
-        }
+let output = document.getElementById("calc")
 
-    })
+function display(num) {
+  output.value += num;
 }
 
+function Clear() {
+  output.value = "";
+}
+
+function Delete() {
+  output.value = output.value.slice(0, -1);
+}
+
+function calc() {
+  try {
+    output.value = eval(output.value);
+  }
+  catch(err) {
+    output.value = "Error";
+  }
+}
